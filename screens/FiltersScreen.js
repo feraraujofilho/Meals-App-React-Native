b936fe7b736e84802f9b8ee528650972964159f5
Fernando Araujo <fernando.araujo.filho@tenderframe.de>
2020-09-30 15:57:22 +0200

    toggles

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,12 +1,34 @@
-import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import React, { useState } from "react"
+import { Platform, StyleSheet, Switch, Text, View } from "react-native"
 import { HeaderButtons, Item } from "react-navigation-header-buttons"
 import CustomHeaderButton from "../components/HeaderButton"
+import Colors from "../constants/Colors"
+
+const FilterSwitch = (props) => {
+    return (
+        <View style={styles.filterContainer}>
+            <Text>{props.label}</Text>
+            <Switch trackColor={{ true: Colors.primaryColor }}
+                value={props.state}
+                thumbColor={Platform.OS === "android" ? Colors.primaryColor : ""}
+                onValueChange={props.onChange} />
+        </View>
+    )
+}
 
 const FiltersScreen = props => {
+    const [isGlutenFree, setIsGlutenFree] = useState(false)
+    const [isLactoseFree, setIsLactoseFree] = useState(false)
+    const [isVegan, setIsVegan] = useState(false)
+    const [isVegetarian, setIsVegetarian] = useState(false)
+
     return (
-        <View>
-            <Text>The Filters Screen</Text>
+        <View style={styles.screen}>
+            <Text style={styles.title}>Available Filters / Restrictions</Text>
+            <FilterSwitch label="Gluten-free" state={isGlutenFree} onChange={(newValue) => setIsGlutenFree(newValue)} />
+            <FilterSwitch label="Lactose-free" state={isLactoseFree} onChange={(newValue) => setIsLactoseFree(newValue)} />
+            <FilterSwitch label="Vegan" state={isVegan} onChange={(newValue) => setIsVegan(newValue)} />
+            <FilterSwitch label="Vegetarian" state={isVegetarian} onChange={(newValue) => setIsVegetarian(newValue)} />
         </View>
     )
 }
@@ -20,6 +42,24 @@ FiltersScreen.navigationOptions = navData => {
     }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        alignItems: "center",
+    },
+    title: {
+        fontFamily: "open-sans-bold",
+        fontSize: 22,
+        margin: 20,
+        textAlign: "center"
+    },
+    filterContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "80%",
+        marginVertical: 15
+    }
+})
 
 export default FiltersScreen
